b9cccfb79cc349eb5eebc104dc42f87b7ec688df
Jirawat <nuanlaong_j@su.ac.th>
2025-08-28 11:53:09 +0700

    lab5

---
+++ homie/src/App.jsx
@@ -0,0 +1,20 @@
+// import logo from './Vue_logo.svg';
+// `from src path` import Hellu from "./Hellu"
+// `from src path` import Badbye from "./Badbye"
+// import JSXExamples from './components/JSXExamples';
+import React from 'react';
+import './App.css';
+import ComponentExamples from './components/ComponentExamples';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Hellu/>
+      <JSXExamples/>
+      <Badbye/> */}
+      <ComponentExamples/>
+    </div>
+  );
+}
+
+export default App;
